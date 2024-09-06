cfe0d8aeedc344f615fcfe534d9c5b7607435271
Steve Aldworth <s.aldworth@tvdsb.ca>
2024-09-06 08:42:35 -0400

    Completed the assignment

--- GitHubDemo1/Program.cs
+++ GitHubDemo1/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
+            // Aldworth
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("Finished, 100%");
         }
     }
 }
